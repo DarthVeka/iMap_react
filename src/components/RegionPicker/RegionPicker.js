d15bacb6598cbf603f8f989a86d75a07dad5d825
darth.veka <vedran.von.vitez@gmail.com>
2018-03-10 19:00:28 +0100

    Created basic project layout

---
+++ src/components/RegionPicker/RegionPicker.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Map from '../Map/Map';
+
+class RegionPicker extends Component {
+    render() {
+        return (
+            <div>
+                blago meni
+                <Map />
+            </div>
+        );
+    }
+}
+
+export default RegionPicker;
