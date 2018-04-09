a5d9a8ff1ad70bc28574b4d9bb3d07a912bcef90
Darth.Veka <vedran.von.vitez@gmail.com>
2018-04-09 09:25:22 +0200

    Fixed bugs with map centering and street view exiting

--- src/containers/Map/GoogleMapContainer/GoogleMap.js
+++ src/containers/Map/GoogleMapContainer/GoogleMap.js
@@ -6,79 +6,23 @@ import * as actions from '../../../store/actions';
 import classes from './GoogleMap.css';
 
 export class GoogleMap extends Component {
+    
+    state = {
+        map: null
+    }
 
     componentWillMount() {
         this.props.startFetchingData(this.props.selectedRegion.id);
     }
-    // componentDidUpdate() {
-    //     if (!this.props.loading && this.props.mapPoiData !== null) {
-    //         this.setMap();
-    //     }
-    // }
-
-    //     setMap = () => {
-    //         // SET MAP
-    //         const map = new window.google.maps.Map(this.refs.map, {
-    //             zoom: 9,
-    //             center: this.props.mapPoiData.center,
-    //             gestureHandling: 'cooperative',
-    //             zoomControl: false,
-    //             mapTypeControl: false,
-    //             scaleControl: false,
-    //             streetViewControl: true,
-    //             streetViewControlOptions: {
-    //                 position: window.google.maps.ControlPosition.BOTTOM_CENTER
-    //             },
-    //             rotateControl: false,
-    //             fullscreenControl: false
-    //         });
-
-    //         // SET ALL POIS
-    //         this.props.mapPoiData.pois.map(poi => {
-    //              const marker = new window.google.maps.Marker({
-    //                 position: poi.coordinates,
-    //                 animation: window.google.maps.Animation.BOUNCE,
-    //                 map: map
-    //             });
-    //             return marker;
-    //         })
-
-    //         // DISABLE UI ON STREET VIEW
-    //         map.getStreetView().setOptions({
-    //             disableDefaultUI: true,
-    //             enableCloseButton: false
-    //         })
-
-    //         const centerMap = (map) => {
-    //             map.panTo(this.props.mapPoiData.center);
-    //         }
-
-    // ////////////////////////////////////////////////////
-    //         // map.addListener('center_changed', () => {
-    //         //     window.setTimeout(() => {
-    //         //         map.panTo(this.props.mapPoiData.center);
-    //         //     }, 5000);
-    //         // });
-
-
-    //         // window.google.maps.event.addListener(map.getStreetView(),'pov_changed',() => {
-    //         //     setTimeout(() => {
-    //         //         map.getStreetView().setVisible(false)
-    //         //     }, 10000) 
-    //         //  });
-    //     }
-
-    centerMap = (map) => {
-        map.panTo(this.props.mapPoiData.center);
+    componentDidUpdate() {
+        if (!this.props.loading && this.props.mapPoiData !== null && this.state.map === null) {
+            this.setMap();
+        }
     }
 
-
-    render() {
-
-        let map = null;
-
-        if (!this.props.loading && this.props.mapPoiData !== null) {
-            map = new window.google.maps.Map(this.refs.map, {
+        setMap = () => {
+            // SET MAP
+             const map = new window.google.maps.Map(this.refs.map, {
                 zoom: 9,
                 center: this.props.mapPoiData.center,
                 gestureHandling: 'cooperative',
@@ -95,7 +39,7 @@ export class GoogleMap extends Component {
 
             // SET ALL POIS
             this.props.mapPoiData.pois.map(poi => {
-                const marker = new window.google.maps.Marker({
+                 const marker = new window.google.maps.Marker({
                     position: poi.coordinates,
                     animation: window.google.maps.Animation.BOUNCE,
                     map: map
@@ -108,11 +52,21 @@ export class GoogleMap extends Component {
                 disableDefaultUI: true,
                 enableCloseButton: false
             })
+
+            this.setState({
+                map: map
+            });       
         }
 
+    centerMap = () => {
+        this.state.map.panTo(this.props.mapPoiData.center);
+    }
 
+    exitStreetView = () => {
+        this.state.map.getStreetView().setVisible(false);
+    }
 
-
+    render() {
 
         let spinner = <Spinner />
 
@@ -124,8 +78,8 @@ export class GoogleMap extends Component {
             <Fragment>
                 {spinner}
                 <div className={classes.GoogleMap} ref="map" />
-                <button onClick={() => this.centerMap(map)}>Center</button>
-                <button>Exit street view</button>
+                <button onClick={this.centerMap}>Center</button>
+                <button onClick={this.exitStreetView}>Exit street view</button>
             </Fragment>
 
         )
